4895c0c86a215f92157bdef56248fbebae25d826
vicky-playground <90204593+vicky-playground@users.noreply.github.com>
2023-08-30 00:11:45 -0400

    use watsonx LLM

---
+++ server.py
@@ -0,0 +1,27 @@
+import base64
+import json
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return None
+
+
+@app.route('/speech-to-text', methods=['POST'])
+def speech_to_text_route():
+    return None
+
+
+@app.route('/process-message', methods=['POST'])
+def process_message_route():
+    return None
+
+
+if __name__ == "__main__":
+    app.run(port=8000, host='0.0.0.0')
---
+++ worker.py
@@ -0,0 +1,10 @@
+
+def speech_to_text(audio_binary):
+    return None
+
+
+def text_to_speech(text, voice=""):
+    return None
+
+def watsonx_process_message(user_message):
+    return None
